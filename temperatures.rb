352905d5c3290a4afd2cc08625710abd6c84218e
xcudder <xcudder@hotmail.com>
2021-02-17 02:04:48 -0300

    Slightly organize temperature converter logic into classes

--- temperatures.rb
+++ temperatures.rb
@@ -1,23 +1,34 @@
-def celsius_to_fahrenheit(celsius)
-	(celsius.to_f * 9/5) + 32
+class Converter
+	@option = 0
+
+	def celsius_to_fahrenheit(temperature)
+		(temperature.to_f * 9/5) + 32
+	end
+
+	def fahrenheit_to_celsius(temperature)
+		(temperature − 32) * 5/9
+	end
+
+	def show_menu()
+		loop do
+			puts "Converter\n1 - celsius to fahrenheit\n2 - fahrenheit to celsius"
+			@option = gets.to_i
+			break if @option == 1 || @option == 2
+		end
+	end
+
+	def ask_temperature()
+		if(@option == 1)
+			puts "Celsius?"
+			puts celsius_to_fahrenheit(gets)
+		elsif (@option == 2)
+			puts "Fahrenheits?"
+			puts fahrenheit_to_celsius(gets)
+		end
+	end
 end
 
-def fahrenheit_to_celsius(fahrenheit)
-	(fahrenheit − 32) * 5/9
-end
+converter = Converter.new
 
-$option = 0
-
-loop do
-	puts "Converter\n1 - celsius to fahrenheit\n2 - fahrenheit to celsius"
-	$option = gets.to_i
-	break if $option == 1 || $option == 2
-end
-
-if($option == 1)
-	puts "Celsius?"
-	puts celsius_to_fahrenheit(gets)
-elsif ($option == 2)
-	puts "Fahrenheits?"
-	puts fahrenheit_to_celsius(gets)
-end
+converter.show_menu()
+converter.ask_temperature()
