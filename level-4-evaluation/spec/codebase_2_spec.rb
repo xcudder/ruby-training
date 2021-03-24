4fd7edc1651c7aa8587defe424b9057563331db2
Math Costa <xcudder@hotmail.com>
2021-03-24 16:00:50 -0300

    Adds spec file for codebase 2

---
+++ level-4-evaluation/spec/codebase_2_spec.rb
@@ -0,0 +1,19 @@
+require_relative '../codebase_2'
+
+describe TemperatureConverter do
+    describe ".convert_to_celsius" do
+        context "given a number" do
+            it "should presume celsius and turn it to fahrenheit" do
+                expect(TemperatureConverter::convert_to_celsius(77)).to eq(25)
+            end
+        end
+    end
+
+    describe ".convert_to_fahrenheit" do
+        context "given a number" do
+            it "should presume fahrenheit and turn it to celsius" do
+                expect(TemperatureConverter::convert_to_fahrenheit(25)).to eq(77)
+            end
+        end
+    end
+end
